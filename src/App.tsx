cda1e895bbae6819adbd78aec6501b465a3f018a
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-12-19 15:27:45 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import './styles/main.scss'
 
 function App() {
   return (
+  <>
     <Header isSearchBar={true} />
+    <Footer />
+  </>
+
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TextLogo from './UI/TextLogo';
+
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className="wrapper">
+        <div className="footer__container">
+          <TextLogo />
+          <p>2022 <a href="https://github.com/avpankov" className='link' target="_blank" rel="noreferrer">github</a></p>
+          <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
+            <img src="/assets/logos/rs_school_js.svg" alt="RS School course logo" height="60px" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
