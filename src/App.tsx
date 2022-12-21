6f1bd227fd03727f4a31e2bcae95120dc9a89cf2
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-12-21 09:45:22 +0300

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 import ProductsPage from "./pages/ProductsPage";
 import ShoppingCartPage from "./pages/ShoppingCartPage";
 import "./styles/main.scss";
@@ -7,9 +8,10 @@ import "./styles/main.scss";
 function App() {
   return (
     <Routes>
-    <Route path='/' element={<ProductsPage/>}/>
-    <Route path='/cart' element={<ShoppingCartPage/>}/>
-  </Routes>
+      <Route path='/' element={<ProductsPage />} />
+      <Route path='/cart' element={<ShoppingCartPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="not-found wrapper">
+      <h1>Page not found(404)</h1>
+    </div>
+  );
+};
+
+export default NotFound;
