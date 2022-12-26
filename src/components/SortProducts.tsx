14f76d6f61a340a226dc15033fd0dcf25c654100
Uladzimir <vl.barbikov@gmail.com>
2022-12-26 21:13:40 +0300

    fix: change toggle view mode

--- src/components/SortProducts.tsx
+++ src/components/SortProducts.tsx
@@ -1,44 +1,32 @@
 import React from "react";
 import ViewOptions from "./ViewOptions";
-import { useState, useRef } from "react";
+
+export enum ActiveMode {
+  big = "big",
+  small = "small",
+}
 
 interface ISortProducts {
   amount: number;
   setValueSort: React.Dispatch<React.SetStateAction<string>>;
   valueSort: string;
-  isBigViewMode: () => void;
+  activeMode: string;
+  setActiveMode: React.Dispatch<React.SetStateAction<ActiveMode>>;
 }
 
 const SortProducts = ({
   amount,
   setValueSort,
   valueSort,
-  isBigViewMode,
+  activeMode,
+  setActiveMode,
 }: ISortProducts) => {
-  const viewBig = useRef<HTMLDivElement>(null);
-  const viewSmall = useRef<HTMLDivElement>(null);
-
-  const [viewModeBig, setviewModeBig] = useState("view-mode_big");
-  const [viewModeSmall, setviewModeSmall] = useState("view-mode_small");
-
   const handleClickModeBig = (event: React.MouseEvent<HTMLElement>) => {
-    if (viewModeBig === "view-mode_big") {
-      setviewModeBig("view-mode_big active");
-      setviewModeSmall("view-mode_small");
-      event.currentTarget.className = "view-mode_big active";
-      if (viewSmall.current) viewSmall.current.className = "view-mode_small";
-      isBigViewMode();
-    }
+    setActiveMode(ActiveMode.big);
   };
 
   const handleClickModeSmall = (event: React.MouseEvent<HTMLElement>) => {
-    if (viewModeSmall === "view-mode_small") {
-      setviewModeBig("view-mode_big");
-      setviewModeSmall("view-mode_small active");
-      event.currentTarget.className = "view-mode_small active";
-      if (viewBig.current) viewBig.current.className = "view-mode_big";
-      isBigViewMode();
-    }
+    setActiveMode(ActiveMode.small);
   };
 
   return (
@@ -47,9 +35,10 @@ const SortProducts = ({
       <div className="amount-products">Found: {amount}</div>
       <div className="view-mode">
         <div
-          className="view-mode_big active"
+          className={`view-mode_big ${
+            activeMode === ActiveMode.big ? " active" : ""
+          }`}
           onClick={handleClickModeBig}
-          ref={viewBig}
         >
           <div className="view-mode_big-sqare"></div>
           <div className="view-mode_big-sqare"></div>
@@ -62,9 +51,10 @@ const SortProducts = ({
           <div className="view-mode_big-sqare"></div>
         </div>
         <div
-          className="view-mode_small"
+          className={`view-mode_small ${
+            activeMode === ActiveMode.small ? " active" : ""
+          }`}
           onClick={handleClickModeSmall}
-          ref={viewSmall}
         >
           <div className="view-mode_small-sqare"></div>
           <div className="view-mode_small-sqare"></div>
