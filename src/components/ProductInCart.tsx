a618cb79360efed5cf1515ef0a76c7165a94bcad
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-12-29 10:38:59 +0300

    refactor: remove optional chaining

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -26,19 +26,19 @@ function ProductInCart({product, cart, i}: IPropsForProductInCart) {
   return (
     <div className="products-in-cart__product">
       <div className="products-in-cart__product__index">{i + 1}</div>
-      <div className="products-in-cart__product__thumbnail"><img src={product?.thumbnail} alt={product?.title} /></div>
+      <div className="products-in-cart__product__thumbnail"><img src={product.thumbnail} alt={product.title} /></div>
       <div className="products-in-cart__product__description">
-        <p>{product?.title}</p>
-        <p>{product?.description}</p>
-        <p><SvgSelector id={"star"} />{product?.rating}</p>
+        <p>{product.title}</p>
+        <p>{product.description}</p>
+        <p><SvgSelector id={"star"} />{product.rating}</p>
       </div>
       <div className="products-in-cart__product__price">
         <p>€ {((product!.price * 100) / (100 - product!.discountPercentage)).toFixed(2)}</p>
-        <h3>€ {product?.price}</h3>
+        <h3>€ {product.price}</h3>
         <p>-{product!.discountPercentage.toFixed(0)}%</p>
       </div>
       <div className="products-in-cart__product__amount">
-        <p>In stock: {product?.stock}</p>
+        <p>In stock: {product.stock}</p>
         <div className='product-amount-input'>
           <button className='product-amount-input__btn' onClick={() => handleClickDec(product)}>–</button>
           <input id="amountOfProduct" defaultValue={cart.find(el => product!.id === el.id)?.count} disabled />
